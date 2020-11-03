3a20878941ae0bec78ed6b38c3cfe9987f414ada
JuanG <juangiacosabrk@gmail.com>
2020-11-03 11:06:03 -0300

    Final Commit, got carried out a little bit and forgot to commit. I'll improve at it
        - Grid is initially populated with 256 boxes (16*16)
        - Boxes gain sketchability
        - User can clear grid
        - User is prompted for number of boxes to draw
        - boxes are drawn in the same space as initial

--- etch.js
+++ etch.js
@@ -1,12 +1,83 @@
 const grid = document.querySelector('#container');
-for (let i = 0; i < 256; i++){
-    const divvie = document.createElement('div');
-      divvie.classList.add('box');
-      grid.appendChild(divvie); 
+const clearBtn = document.querySelector('#clear')
+let divs = document.getElementsByClassName('box');
+
+// h & w of container
+const WIDTH = 450;
+const HEIGHT= 450;
+
+populateContainer();
+addSketchability();
+clearBtn.addEventListener('click', () => clearBoxes());
+
+/**
+ * Sketchabilize every box
+ * 
+ * Source:
+  * https://www.30secondsofcode.org/blog/s/javascript-arrow-function-event-listeners
+  */
+ function addSketchability(){
+   for (var i = 0; i < divs.length; i++) {
+     divs[i].addEventListener('mouseover', element => element.currentTarget.classList.toggle('sketched'));
+     console.log(`added + ${i}`);
+   }
+}
+
+function clearBoxes(){
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].classList.remove('sketched');
+  }
+  
+  let width = Number(window.prompt("Specify the width for the new canvas (10-100): ")); 
+  let greaterThanAllowed = width > 100;
+
+  if (greaterThanAllowed){
+    alert("Please select a number between 10-100");
+  } else {
+    // set up new canvas 
+    let amountOfNewBoxes = width * width;
+    removeExistentBoxes();
+    // populate grid with the inputed value
+    populateContainer(amountOfNewBoxes);
+    divs = document.getElementsByClassName('box');
+    //change boxes length size
+    resizeBoxes(width);
+    addSketchability();
+
+  }
+}
+
+//Clear all existent divs with id of box
+function removeExistentBoxes(){
+  let container = document.getElementById('container');
+  while ( container.firstChild ) container.removeChild( container.firstChild );
+}
+
+// creates a n*n grid of boxes 
+function populateContainer(n = undefined){
+  
+  if (n == undefined){
+    //Initial population of container with 256 boxes
+    n = 256
   }
 
-  // Sets every box background to black, or sketched
-  const divs = document.getElementsByClassName('box');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('mouseover', element => element.currentTarget.classList.toggle('sketched'));
+  for (let i = 0; i < n; i++){
+    const divvie = document.createElement('div');
+    divvie.classList.add('box');
+    grid.appendChild(divvie); 
+  }
 }
+
+// Given the #of boxes, resize #box i/o to cover the entire container (450*450px)
+function resizeBoxes(numberOfBoxes){
+  let boxSizeLength = WIDTH / numberOfBoxes;
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.width = boxSizeLength;
+    divs[i].style.height = boxSizeLength;
+  }
+}
+
+function sketchBox(){
+  
+}
+
